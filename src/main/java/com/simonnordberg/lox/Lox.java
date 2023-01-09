10db4c0bc2043c74c4e1ea50ab2a2ba02e39e6df
Simon Nordberg <simon@simonnordberg.com>
2023-01-09 17:48:18 +0100

    Add simple interpreter

--- src/main/java/com/simonnordberg/lox/Lox.java
+++ src/main/java/com/simonnordberg/lox/Lox.java
@@ -9,8 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-
+  private static final Interpreter interpreter = new Interpreter();
   private static boolean hadError = false;
+  private static boolean hadRuntimeError = false;
 
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
@@ -49,6 +50,7 @@ public class Lox {
     }
 
     System.out.println(new AstPrinter().print(expression));
+    interpreter.interpret(expression);
   }
 
   private static void runFile(String arg) throws IOException {
@@ -57,6 +59,9 @@ public class Lox {
 
     if (hadError) {
       System.exit(65);
+    }
+    if (hadRuntimeError) {
+      System.exit(70);
     }
   }
 
@@ -75,5 +80,10 @@ public class Lox {
     } else {
       report(token.line, " at '" + token.lexeme + "'", message);
     }
+  }
+
+  public static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
   }
 }
---
+++ src/main/java/com/simonnordberg/lox/RuntimeError.java
@@ -0,0 +1,11 @@
+package com.simonnordberg.lox;
+
+public class RuntimeError extends RuntimeException {
+
+  final Token token;
+
+  public RuntimeError(Token token, String message) {
+    super(message);
+    this.token = token;
+  }
+}
