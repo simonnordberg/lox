743bb827a0f6914b540ea82169485d7e109b5cae
Simon Nordberg <simon@simonnordberg.com>
2024-01-16 10:42:58 +0100

    Updates

--- c/main.c
+++ c/main.c
@@ -1,17 +1,23 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, char **argv) {
-  Chunk chunk;
-  initChunk(&chunk);
-  int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
+    initVM();
 
-  writeChunk(&chunk, OP_RETURN, 123);
+    Chunk chunk;
+    initChunk(&chunk);
 
-  disassembleChunk(&chunk, "test chunk");
-  freeChunk(&chunk);
-  return 0;
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_RETURN, 123);
+
+    disassembleChunk(&chunk, "test chunk");
+    freeVM();
+    freeChunk(&chunk);
+
+    return 0;
 }
